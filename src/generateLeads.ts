cb0e38efaa81a6e55d8fc2a729b10519209520d8
Snichoo <sam.s.nichols2@gmail.com>
2024-10-06 20:06:10 +1100

    down to 7

--- src/generateLeads.ts
+++ src/generateLeads.ts
@@ -1038,7 +1038,7 @@ export async function generateLeads(
         uniqueResults = await runActorPool(
           businessType,
           structuredSuburbs,
-          15,
+          7,
           stateAbbr  // Pass the state abbreviation here
         );
       } else {
